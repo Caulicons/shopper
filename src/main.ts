5c80f0655040582463d16b936206e93da2b2ab3e
Vitor Oliveira. <caulicons@gmail.com>
2024-08-30 14:01:44 -0300

    setting upload limit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   process.env.TZ = '-03:00';
   app.useGlobalPipes(new ValidationPipe());
+
+  // Limit request size to 10MB
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
 
   await app.listen(process.env.PORT || 4000);
 }
