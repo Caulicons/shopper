776cdce298f9449a4ea031a13413069c0d6c3828
Vitor Oliveira. <caulicons@gmail.com>
2024-08-30 14:01:21 -0300

    config dotenv

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! ${process.env.GEMINI_API_KEY}`;
+    return `Hello World!`;
   }
 }
